aea588eda6252493844dccfb62898eee08ba0caa
Vishal Biyani <vishalbiyani@gmail.com>
2025-05-09 19:33:01 +0530

    updated env variables

--- src/config.js
+++ src/config.js
@@ -5,5 +5,10 @@ const config = {
     tokenProxyUrl: process.env.REACT_APP_TOKEN_PROXY_URL,
   }
 };
+console.log('ENV:', {
+  clientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
+  redirectUri: process.env.REACT_APP_REDIRECT_URI,
+  tokenProxyUrl: process.env.REACT_APP_TOKEN_PROXY_URL,
+});
 
 export default config;
