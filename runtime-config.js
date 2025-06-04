b3473de171d3894491fea895a6385bb907155d84
biyani701 <biyani701@users.noreply.github.com>
2025-06-04 10:47:41 +0000

    deploy: d83b5a52b43a68b681338694c8e1d3c731b9d4f6

--- runtime-config.js
+++ runtime-config.js
@@ -9,7 +9,7 @@ window.runtimeConfig = {
   "GOOGLE_CLIENT_ID": "",
   "REDIRECT_URI": "https://vishal.biyani.xyz/callback",
   "TOKEN_PROXY_URL": "https://auth.vishal.biyani.xyz/api/github-token.js",
-  "BUILD_TIMESTAMP": "2025-06-04T06:27:01.011Z",
+  "BUILD_TIMESTAMP": "2025-06-04T10:46:31.550Z",
   "BUILD_VERSION": "0.0.0"
 };
 console.log('[Runtime Config] Loaded with AUTH_SERVER_URL:', window.runtimeConfig.AUTH_SERVER_URL);
